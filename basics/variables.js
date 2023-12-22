1be7ef1ce4f21b30752eb66aded5a8bb16266b6d
K.Gnana Varshita <105348644+GnanaVarshita@users.noreply.github.com>
2023-12-22 07:14:55 +0000

    2nd commit

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const  accountId =49792
+let accountEmail= 'gnanavarshitagv@gmail.com'
+var accountPassword = 'bdkjksd'
+accountEmail='hdsaj@gmail.com'
+let place='India'
+console.log(accountId)
+console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,place])
