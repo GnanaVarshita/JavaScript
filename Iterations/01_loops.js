ce8d646edcc9eb1b81c9142f364b5e99fb65b76d
K.Gnana Varshita <105348644+GnanaVarshita@users.noreply.github.com>
2023-12-25 09:52:26 +0000

    12th commit

---
+++ Iterations/01_loops.js
@@ -0,0 +1,26 @@
+for (let index = 0; index < 10; index++) {
+    const element = index
+    console.log(index)
+    
+}
+
+//inner loop and outer loop
+// nested loop
+
+// for(let i=1;i<=10;i++){
+//     for(let j=1;j<=10;j++){
+//         console.log(`${i}*${j}=${i*j}`)
+//     }
+
+// }
+
+//break and continue
+
+for (let index = 0; index < 20; index++) {
+    if(index==5){
+        console.log('detected 5')
+        break;
+    }
+    console.log(`Values is ${index}`)
+    
+}
---
+++ Iterations/02_while.js
@@ -0,0 +1,25 @@
+// while(condition){
+
+// }
+
+let i=0
+while(i<=10){
+   // console.log(i)
+    i++;
+}
+
+console.log(i)
+let myArray=['IronMan','American','Hulk','Spiderma']
+let j=0
+while(j<myArray.length){
+    console.log(myArray[j])
+    j++;
+}
+
+//do while
+
+let score=11
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score<=10)
