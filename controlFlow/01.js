2e9a03e8964cd610a9a3bb79b9a7899cee12208e
K.Gnana Varshita <105348644+GnanaVarshita@users.noreply.github.com>
2023-12-25 04:22:54 +0000

    11th commit

---
+++ controlFlow/01.js
@@ -0,0 +1,34 @@
+//Crontrol flow or Logic Flow
+const isUserloggedIn = true
+
+if(isUserloggedIn){
+console.log(`Welcome User`)
+}
+//Comparision Check
+// <,>,<=,>=,==,!=,  ===(checks even type)
+
+const n1= '6'
+const n2=6
+if(!(n1===n2)){
+    console.log('type is not same')
+}
+
+if(n1==n2){
+    console.log('value is same')
+}
+
+//Short Hand notation :
+
+const balance=20000
+if(balance>1000) console.log('yes')
+
+//Nested
+
+if(balance>500){
+    console.log('greater than 500')
+    {
+        if(balance>10000){
+            console.log('greater than 10000')
+        }
+    }
+}
