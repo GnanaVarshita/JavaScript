783e73658ee7c226b0ebf25bb9afa931beaf2338
K.Gnana Varshita <105348644+GnanaVarshita@users.noreply.github.com>
2023-12-26 04:55:25 +0000

    13th commit

--- Iterations/03_hiegherorder.js
+++ Iterations/03_hiegherorder.js
@@ -10,3 +10,72 @@ const greetings ='Hello World'
 for (const greet of greetings) {
     console.log(`Each char is ${greet}`)
 }
+
+//Maps
+//map is an object itself , holds key-value pair
+const map = new Map()
+map.set('IN','India')
+map.set('USA', 'United States')
+map.set('FR','France')
+
+console.log(map)
+
+for (const [key,value] of map) {
+    console.log(key, ':-' ,value)
+}
+
+//maps are iteratable but not objects (try forof loop for this myObj)
+const myObj={
+    'game1':'dncsj',
+    'game2':'nsjcn'
+}
+
+
+const myObj1={
+    js : 'javascript',
+    cpp :'C++',
+    rb: 'ruby',
+    swift : 'swift by apple'
+}
+
+//use forin loop
+
+for (const key in myObj1) {
+    console.log(`${key} shortcut is ${myObj1[key]}`)
+}
+
+
+//for each 
+const coding = ['js','ruby','swift','go','java']
+
+coding.forEach(element => {
+    console.log(element)
+});
+
+// for each loop in objects
+
+const obj =[
+{
+    language:'telugu',
+    state:'AP',
+},
+
+{
+    language:'tamil',
+    state:'TN',
+},
+{
+    language:'Kannada',
+    state:'Karnataka',
+},
+{
+    language:'hindi',
+    state:'pura',
+},
+
+
+]
+
+obj.forEach(element => {
+    console.log(element.language);
+});
